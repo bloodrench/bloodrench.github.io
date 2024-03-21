804388d959ea377ce6fdc4fb2eb456766f576278
bloodrench <5ctha671@gmail.com>
2024-03-20 20:00:42 -0700

    Small UI overhaul

--- public/js/app.js
+++ public/js/app.js
@@ -43,9 +43,15 @@ const updateUI = async () => {
         document.getElementById("ipt-user-profile").textContent = JSON.stringify(
             await auth0Client.getUser()
         );
+        await profilePicture();
 
     } else {
         document.getElementById("gated-content").classList.add("hidden");
+    }
+
+    async function profilePicture() {
+        document.getElementById("profile-picture-nav").src = (await auth0Client.getUser()).picture;
+        document.getElementById("profile-picture-nav").style = "width: 50px; height: 50px;";
     }
 };
 updateUI();

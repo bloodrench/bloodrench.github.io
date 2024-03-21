cc4355e607ba786409af769df5a668c429810731
bloodrench <5ctha671@gmail.com>
2024-03-20 20:28:24 -0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -58,7 +58,7 @@ updateUI();
 const login = async () => {
     await auth0Client.loginWithRedirect({
         authorizationParams: {
-            redirect_uri: "https://bloodrench.github.io/nadon"
+            redirect_uri: "https://bloodrench.github.io/"
         }
     });
 };
